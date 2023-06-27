955e3d8ac475857a1161e0d0239b179022a05f22
Aleh Yemelyanchyk <yem1403@gmail.com>
2023-06-27 04:21:48 +0000

    - added beans.xml - configured MainServlet, ServletHelper to use autowired

--- src/main/java/com/virtualpairprogrammers/helper/ServletHelper.java
+++ src/main/java/com/virtualpairprogrammers/helper/ServletHelper.java
@@ -1,17 +1,27 @@
 package com.virtualpairprogrammers.helper;
 
 import com.virtualpairprogrammers.data.DefaultDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.servlet.http.HttpServletRequest;
 
 public class ServletHelper {
 
-    private DefaultDAO dao = new DefaultDAO();
+    private DefaultDAO dao;
 
     public void populateModel(HttpServletRequest request) {
         request.setAttribute("income", "TestIncomeValue");
         request.setAttribute("incomes", dao.findAllIncomes());
         request.setAttribute("outcomes", dao.findAllOutcomes());
         request.setAttribute("outcome", "TestOutcomeValue");
+    }
+
+    public DefaultDAO getDao() {
+        return dao;
+    }
+
+    @Autowired
+    public void setDao(DefaultDAO dao) {
+        this.dao = dao;
     }
 }
--- src/main/java/com/virtualpairprogrammers/servlet/MainServlet.java
+++ src/main/java/com/virtualpairprogrammers/servlet/MainServlet.java
@@ -1,6 +1,7 @@
 package com.virtualpairprogrammers.servlet;
 
 import com.virtualpairprogrammers.helper.ServletHelper;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -13,11 +14,20 @@ import java.io.IOException;
 @WebServlet("/")
 public class MainServlet extends HttpServlet {
 
-    private ServletHelper helper = new ServletHelper();
+    private ServletHelper helper;
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         helper.populateModel(request);
         RequestDispatcher rd = request.getRequestDispatcher("/main.jsp");
         rd.forward(request, response);
+    }
+
+    public ServletHelper getHelper() {
+        return helper;
+    }
+
+    @Autowired
+    public void setHelper(ServletHelper helper) {
+        this.helper = helper;
     }
 }
